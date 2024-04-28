cab6b1f8093bd7b21f6d2325e36150e087b4490c
Gueru <rubencontrerasguerrero@hotmail.com>
2024-04-28 14:45:11 -0500

    feat: test ract-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import './App.css';
-
-const App = () => {
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { Page01 } from './Page01';
+import { Page02 } from './Page02';
+const Home = () => {
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+        <h1>Rsbuild with React</h1>
+        <p>Start building amazing things with Rsbuild.</p>
     </div>
+  );
+}
+const App = () => {
+  return (
+    <Router>
+      <nav className='navBar'>
+        <ul className='navBar__list-link'>
+          <li>
+            <Link className='navBar__link' to="/page01">Página 1</Link>
+          </li>
+          <li>
+            <Link className='navBar__link' to="/page02">Página 2</Link>
+          </li>
+          <li>
+            <Link className='navBar__link' to="/">Home</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/page01" element={<Page01 />} />
+        <Route path="/page02" element={<Page02 />} /> 
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 
